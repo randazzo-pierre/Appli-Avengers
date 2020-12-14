fc1db9ba7127ef821cb8d88e2a9952d69b794787
Thibaut <thibaut.pignon@orange.fr>
2020-12-14 11:21:49 +0100

    add vilainClass extends civilClass

--- Vilain.java
+++ Vilain.java
@@ -1,24 +1,24 @@
 import java.util.*;
 
-public class Hero extends Civil {
-    private String nomHero;
+public class Vilain extends Civil {
+    private String nomVilain;
     private String pouvoir;
     private String pointFaible;
-    private int score;
-    private int nbMissionEff;
-    private String commentaireHero;
+    private int danger;
+    private int nbCrime;
+    private String commentaireVilain;
 
-    public Hero(String nomHero, String nom, String prenom, String civilite, String adresse, String ville, String zip,
+    public Vilain(String nomVilain, String nom, String prenom, String civilite, String adresse, String ville, String zip,
                 String pays, String email, int tel, Date dateNais, String nationnalite, ArrayList<String> organisation,
                 Date dateMort, String commentaireCivil, Date dateAjout, Date dateDerniereModif, int nbIncident,
-                int nbMission, String pouvoir, String pointFaible, int score, int nbMissionEff, String commentaireHero) {
+                int nbMission, String pouvoir, String pointFaible, int score, int nbCrime, String commentaireVilain) {
         super(nom, prenom, civilite, adresse, ville, zip, pays, email, tel, dateNais, nationnalite, organisation,
                 dateMort, commentaireCivil, dateAjout, dateDerniereModif, nbIncident, nbMission);
-        this.nomHero = nomHero;
+        this.nomVilain = nomVilain;
         this.pouvoir = pouvoir;
         this.pointFaible = pointFaible;
-        this.score = score;
-        this.nbMissionEff = nbMissionEff;
-        this.commentaireHero = commentaireHero;
+        this.danger = danger;
+        this.nbCrime = nbCrime;
+        this.commentaireVilain = commentaireVilain;
     }
 }
