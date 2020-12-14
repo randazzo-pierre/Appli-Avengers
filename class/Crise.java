9442c37978fafd7575d8ccb9b7388a713c5536ad
Béatrice <beatrice.briand@viacesi.fr>
2020-12-14 14:40:17 +0100

    commit branche crise

---
+++ class/Crise.java
@@ -0,0 +1,77 @@
+import java.util.*;
+
+public class Crise {
+    private String nomCr;
+    private String typeCr;
+    private String cibleCr;
+    private String descriptionCr;
+
+    /**
+     * @param nomCr
+     * @param typeCr
+     * @param cibleCr
+     * @param descriptonCr
+     */
+
+     public Crise(String nomCr, String typeCr, String cibleCr, String descriptionCr){
+         this. nomCr = nomCr;
+         this.typeCr = typeCr;
+         this.cibleCr = cibleCr;
+         this.descriptionCr = descriptionCr;
+     }
+
+/**
+ * @return String
+ */
+public String getNomCr(){
+    return nomCr;
+}
+
+/**
+ * @return String
+ */
+public String getTypeCr(){
+    return typeCr;
+}
+
+/**
+ * @return String
+ */
+public String getCibleCr(){
+    return cibleCr;
+}
+
+/**
+ * @return String
+ */
+public String getDescriptionCr(){
+    return descriptionCr;
+}
+
+/**
+ * @param nomCr
+ */
+public void setNomCr(String nomCr){
+    this.nomCr = nomCr;
+}
+
+/**
+ * @param typeCr
+ */
+public void setTypeCr(String typeCr){
+    this.typeCr = typeCr;
+}
+/**
+ * @param cibleCr
+ */
+public void setCibleCr(String cibleCr){
+    this.cibleCr = cibleCr;
+}
+/**
+ * @param descritpionCr
+ */
+public void setDesccritpionCr(String descritpionCr){
+    this.descriptionCr = descritpionCr;
+}
+
+}
