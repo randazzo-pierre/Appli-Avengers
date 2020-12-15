867a159924ae89608a769023b451e9af12e501b8
Béatrice <beatrice.briand@viacesi.fr>
2020-12-15 10:44:37 +0100

    connexion AWS

--- MysqlCon.java
+++ MysqlCon.java
@@ -1,13 +1,18 @@
 import java.sql.*;
 public class MysqlCon {
+
+ private static String dbhost = "rds-mysql-avengersapp.cdx9i8eyllsk.eu-west-3.rds.amazonaws.com" ;
+ private static String dbuser = "dbroot" ;
+ private static String dbpwd = "QeTuZ2LFJfSqtbpe" ;
+
     public static void main(String args[]){
         try{
         Class.forName("com.mysql.jdbc.Driver");
         Connection con=DriverManager.getConnection(
-            "jdbc:mysql://localhost:3306/test","root","");
+            "jdbc:mysql://"+dbhost+":3306/BDD_AVENGERS_DEV",dbuser,dbpwd);
       //here sonoo is database name, root is username and password
         Statement stmt=con.createStatement();
-        ResultSet rs=stmt.executeQuery("select * from emp");
+        ResultSet rs=stmt.executeQuery("select * from USER");
         while(rs.next())
         System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));
         con.close();
